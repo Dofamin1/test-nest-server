d6b68a09ae4b612392cd12c517eda61b081f09e1
Anton Kovalenko <akovalenko@elitex.systems>
2024-07-12 18:13:45 +0300

    updated swagger docks

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,13 +1,26 @@
 import {Length, IsEmail, IsNumber, IsOptional} from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class UpdateUserDto {
+  @ApiProperty({
+    example: 12,
+    required: false
+  })
   @IsNumber()
   readonly userId: number;
 
+  @ApiProperty({
+    example: 'John',
+    required: false
+  })
   @IsOptional()
   @Length(3, 20)
   readonly username: string;
 
+  @ApiProperty({
+    example: 'john@gmail.com',
+    required: false
+  })
   @IsOptional()
   @IsEmail()
   readonly email: string;
